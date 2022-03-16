ffa0c92476b3bca65735f2689cd5590d3b73a688
Abner Machado <abnermachado@sisconsultoria.com.br>
2022-03-16 10:48:54 -0300

    terceira alteração

--- src/br/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/br/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,5 +1,4 @@
 package br.com.alura.jdbc.factory;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -9,23 +8,22 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
-	
-
 	public DataSource dataSource;
 
 	public ConnectionFactory() {
 		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
 		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
 		comboPooledDataSource.setUser("root");
-		comboPooledDataSource.setPassword("2410");
-		
-		comboPooledDataSource.setMaxPoolSize(15);
+		comboPooledDataSource.setPassword("root");
 
 		this.dataSource = comboPooledDataSource;
-
 	}
 
-	public Connection recuperarConexao() throws SQLException {
-		return this.dataSource.getConnection();
+	public Connection recuperarConexao() {
+		try {
+			return this.dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
